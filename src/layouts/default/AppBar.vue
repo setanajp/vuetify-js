e881ec9380f632b709606415cdcbcdfd0ea9e9e5
konishi <konishi@tdssap.co.jp>
2023-09-03 17:28:02 +0900

    v-navigation-drawer 追加

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,21 @@
 <template>
   <v-app-bar flat>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     <v-app-bar-title> v-app-bar-title </v-app-bar-title>
     <v-tabs>
       <v-tab v-for="col in menuItem" :to=col[1] exact> {{ col[0] }} </v-tab>
     </v-tabs>
   </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer">
+    <v-list-item v-for="col in menuItem" @click.stop="drawer = false" :to=col[1] exact> {{ col[0] }} </v-list-item>
+  </v-navigation-drawer>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+const drawer = ref(false);
 const menuItem = [
   ['view00', '/'],
   ['view01', '/view01'],
