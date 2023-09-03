cb1fbfdaf031bdcba0225cc2940f6c3eb7e1b71d
konishi <konishi@tdssap.co.jp>
2023-09-03 17:23:36 +0900

    v-tabs 追加

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,9 +1,18 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title> v-app-bar-title </v-app-bar-title>
+    <v-tabs>
+      <v-tab v-for="col in menuItem" :to=col[1] exact> {{ col[0] }} </v-tab>
+    </v-tabs>
   </v-app-bar>
 </template>
 
 <script setup>
-  //
+const menuItem = [
+  ['view00', '/'],
+  ['view01', '/view01'],
+  ['view02', '/view02'],
+  ['view03', '/view03'],
+  ['about', '/about'],
+];
 </script>
